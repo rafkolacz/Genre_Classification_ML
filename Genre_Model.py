e0a28a92bf62347deb477423c3c8b9cee8c3cc98
rafal <rafalrobertkolacz@gmail.com>
2020-03-19 22:09:34 +0100

    conf matrix

--- Genre_Model.py
+++ Genre_Model.py
@@ -7,8 +7,9 @@ import pickle
 import matplotlib.pyplot as pyplot
 from matplotlib import style
 from sklearn import linear_model, preprocessing
+from sklearn.metrics import confusion_matrix
 
-data = pd.read_csv("music.data")
+data = pd.read_csv("music2.data")
 
 predict = "Genre"
 
@@ -23,3 +24,12 @@ model.fit(x_train,y_train)
 
 acc = model.score(x_test,y_test)
 print(acc)
+
+predicted = model.predict(x_test)
+names = ["blues", "classical", "country", "disco", "hiphop", "jazz", "metal", "pop", "reggae", "rock"]
+for x in range(len(predicted)):
+    print("Predicted: ", predicted[x], "Actual: ", y_test[x])
+
+conf = confusion_matrix(predicted, y_test, labels=names)
+print(conf)
+
