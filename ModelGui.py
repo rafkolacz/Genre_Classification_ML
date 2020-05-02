bc41abeffec065aeff1cedea759127f8d15fe8f6
rafal <rafalrobertkolacz@gmail.com>
2020-05-02 14:42:05 +0200

    gui - alpha 0.2

--- ModelGui.py
+++ ModelGui.py
@@ -5,10 +5,24 @@ from tkinter.filedialog import askopenfilename
 filename = ''
 
 
+
+def cleanLabel():
+    #location_str = StringVar()
+    #location_str.set(" ")
+    global location_str2
+    myLabel4 = Label(root, textvariable=location_str2)
+    myLabel4.grid(row=3, column=1)
+
+
 def openFile():
     global filename
+    global location_str2
+    location_str = StringVar()
     filename = askopenfilename()
-    myLabel4 = Label(root, text=filename)
+    if not location_str2:
+        cleanLabel()
+    location_str2.set(filename)
+    myLabel4 = Label(root, textvariable=location_str2)
     myLabel4.grid(row=3, column=1)
     return 0
 
@@ -19,7 +33,6 @@ def genrePred():
         myLabel4.grid(row=3, column=1)
     else:
         genre = audio.prediction(filename)
-        print(genre)
         myLabel5 = Label(root, text="Songs genre is: " + genre)
         myLabel5.grid(row=6, column=1)
 
@@ -29,6 +42,7 @@ root = Tk()
 root.title("Genre Predictor")
 root.resizable(False, False)
 root.geometry("400x140+900+400")
+location_str2 = StringVar()
 
 myLabel1 = Label(root, text=" Hello, welcome in Genre Predictor!")
 myLabel1.grid(row=1, column=1)
